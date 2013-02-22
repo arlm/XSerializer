cf5e97e15e7f90b45642ee50acdc1e9d73a48eb4
Brian Friesen <brian.friesen@gmail.com>
2013-02-21 21:11:22 -0500

    Add support for XmlIgnoreAttribute.

---
+++ XSerializer.Tests/IgnoreAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace XSerializer.Tests
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    public class IgnoreAttributeTests : ObjectToXmlWithDefaultComparison
+    {
+        protected override IEnumerable<TestCaseData> GetTestCaseData()
+        {
+            yield return new TestCaseData(new Foo { Bar = 1, Baz = 2 }, typeof(Foo)).SetName("XmlIgnoreAttribute causes property to be skipped during serialization");
+        }
+
+        public class Foo
+        {
+            [XmlIgnore]
+            public int Bar { get; set; }
+            public int Baz { get; set; }
+        }
+    }
+}
