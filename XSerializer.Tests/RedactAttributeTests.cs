e6e6c37a814d3c464f4110caca6799bc9c8bb40b
Brian Friesen <brianfriesen@quickenloans.com>
2013-05-02 22:21:32 -0400

    Added redact attribute.

---
+++ XSerializer.Tests/RedactAttributeTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace XSerializer.Tests
+{
+    public class RedactAttributeTests
+    {
+        [Test]
+        public void LettersAreRedacted()
+        {
+            var input = "abcXYZ";
+            var attribute = new RedactAttribute();
+
+            var redacted = attribute.Redact(input);
+
+            Assert.That(redacted, Is.EqualTo("XXXXXX"));
+        }
+
+        [Test]
+        public void NumbersAreRedacted()
+        {
+            var input = "123789";
+            var attribute = new RedactAttribute();
+
+            var redacted = attribute.Redact(input);
+
+            Assert.That(redacted, Is.EqualTo("######"));
+        }
+
+        [Test]
+        public void NonAlphanumericsAreNotRedacted()
+        {
+            var input = @"!%&( +~?<|{]\";
+            var attribute = new RedactAttribute();
+
+            var redacted = attribute.Redact(input);
+
+            Assert.That(redacted, Is.EqualTo(input));
+        }
+
+        [Test]
+        public void OnlyPartsMatchingRedactPatternAreRedacted()
+        {
+            var input = "123-45-6789";
+            var attribute = new RedactAttribute(@"\d{3}-\d{2}-");
+
+            var redacted = attribute.Redact(input);
+
+            Assert.That(redacted, Is.EqualTo("###-##-6789"));
+        }
+    }
+}
---
+++ XSerializer/RedactAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace XSerializer
+{
+    public class RedactAttribute : Attribute
+    {
+        private static readonly Regex Numbers = new Regex(@"\d", RegexOptions.Compiled);
+        private static readonly Regex Letters = new Regex(@"[a-zA-Z]", RegexOptions.Compiled);
+
+        private readonly Regex _redactRegex;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedactAttribute"/> class.
+        /// </summary>
+        public RedactAttribute()
+            : this(".*")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedactAttribute"/> class. 
+        /// </summary>
+        /// <param name="redactPattern">
+        /// </param>
+        public RedactAttribute(string redactPattern)
+        {
+            _redactRegex = new Regex(redactPattern, RegexOptions.Singleline | RegexOptions.Compiled);
+        }
+
+        /// <summary>
+        /// Redacts the clear-text.
+        /// </summary>
+        /// <param name="clearText">Some clear-text.</param>
+        /// <returns>The redacted text.</returns>
+        public string Redact(string clearText)
+        {
+            return _redactRegex.Replace(clearText, match => Letters.Replace(Numbers.Replace(match.Value, "#"), "X"));
+        }
+    }
+}
