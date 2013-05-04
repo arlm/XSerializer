330a1689adac7ca7b591c0f5bcf32c951a876647
Brian Friesen <brianfriesen@quickenloans.com>
2013-05-03 20:16:00 -0400

    Moved OptionsExtensions to its own file.

--- XSerializer/IXmlSerializerOptions.cs
+++ XSerializer/IXmlSerializerOptions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace XSerializer
 {
@@ -9,40 +7,5 @@ namespace XSerializer
         string DefaultNamespace { get; }
         Type[] ExtraTypes { get; }
         string RootElementName { get; }
-    }
-
-    public static class OptionsExtensions
-    {
-        public static IXmlSerializerOptions WithRootElementName(this IXmlSerializerOptions options, string rootElementName)
-        {
-            return new Options
-            {
-                DefaultNamespace = options.DefaultNamespace,
-                ExtraTypes = options.ExtraTypes,
-                RootElementName = rootElementName
-            };
-        }
-
-        public static IXmlSerializerOptions WithAdditionalExtraTypes(this IXmlSerializerOptions options, params Type[] additionalExtraTypes)
-        {
-            return options.WithAdditionalExtraTypes((IEnumerable<Type>)additionalExtraTypes);
-        }
-
-        public static IXmlSerializerOptions WithAdditionalExtraTypes(this IXmlSerializerOptions options, IEnumerable<Type> additionalExtraTypes)
-        {
-            return new Options
-            {
-                DefaultNamespace = options.DefaultNamespace,
-                ExtraTypes = (options.ExtraTypes ?? new Type[0]).Concat(additionalExtraTypes).Distinct().ToArray(),
-                RootElementName = options.RootElementName
-            };
-        }
-
-        private class Options : IXmlSerializerOptions
-        {
-            public string DefaultNamespace { get; set; }
-            public Type[] ExtraTypes { get; set; }
-            public string RootElementName { get; set; }
-        }
     }
 }
---
+++ XSerializer/OptionsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XSerializer
+{
+    public static class OptionsExtensions
+    {
+        public static IXmlSerializerOptions WithRootElementName(this IXmlSerializerOptions options, string rootElementName)
+        {
+            return new XmlSerializerOptions
+            {
+                DefaultNamespace = options.DefaultNamespace,
+                ExtraTypes = options.ExtraTypes,
+                RootElementName = rootElementName
+            };
+        }
+
+        public static IXmlSerializerOptions WithAdditionalExtraTypes(this IXmlSerializerOptions options, params Type[] additionalExtraTypes)
+        {
+            return options.WithAdditionalExtraTypes((IEnumerable<Type>)additionalExtraTypes);
+        }
+
+        public static IXmlSerializerOptions WithAdditionalExtraTypes(this IXmlSerializerOptions options, IEnumerable<Type> additionalExtraTypes)
+        {
+            return new XmlSerializerOptions
+            {
+                DefaultNamespace = options.DefaultNamespace,
+                ExtraTypes = (options.ExtraTypes ?? new Type[0]).Concat(additionalExtraTypes).Distinct().ToArray(),
+                RootElementName = options.RootElementName
+            };
+        }
+
+        private class XmlSerializerOptions : IXmlSerializerOptions
+        {
+            public string DefaultNamespace { get; set; }
+            public Type[] ExtraTypes { get; set; }
+            public string RootElementName { get; set; }
+        }
+    }
+}
