6ea4ece2707ca78b28199eb0ecad070abc36dd8e
Brian Friesen <brianfriesen@quickenloans.com>
2014-08-21 21:04:04 -0400

    Run msbuild before nuget pack.

--- build/buildNuget.XSerializer.WebApi.bat
+++ build/buildNuget.XSerializer.WebApi.bat
@@ -1,1 +1,2 @@
+msbuild /p:Configuration=Release ..\XSerializer.WebApi\XSerializer.WebApi.csproj
 nuget pack ..\XSerializer.WebApi\XSerializer.WebApi.csproj -Properties Configuration=Release
--- build/buildNuget.XSerializer.bat
+++ build/buildNuget.XSerializer.bat
@@ -1,1 +1,2 @@
+msbuild /p:Configuration=Release ..\XSerializer\XSerializer.csproj
 nuget pack ..\XSerializer\XSerializer.csproj -Properties Configuration=Release
