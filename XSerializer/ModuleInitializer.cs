d2580aff9376b9f0eee416cb614d36de65114b99
Brian Friesen <brianfriesen@quickenloans.com>
2014-12-17 17:54:35 -0500

    Implement better mechanism for specifying an encryption provider.

--- XSerializer/ModuleInitializer.cs
+++ XSerializer/ModuleInitializer.cs
@@ -9,24 +9,17 @@ namespace XSerializer
 {
     public static class ModuleInitializer // Future devs: Do not change the name of this class
     {
+        private static readonly string _iEncryptionProviderName = typeof(IEncryptionProvider).AssemblyQualifiedName;
+        private static readonly string _iEncryptionProviderProviderName = typeof(IEncryptionProviderProvider).AssemblyQualifiedName;
+
         public static void Run() // Future devs: Do not change the signature of this method
         {
-            var iEncryptionProviderProviderName = typeof(IEncryptionProviderProvider).AssemblyQualifiedName;
+            SetCurrentEncryptionProvider();
+        }
 
-            if (iEncryptionProviderProviderName == null)
-            {
-                return;
-            }
-
-            IEnumerable<string> files;
-
-            try
-            {
-                files =
-                    Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
-                    .Concat(Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.exe"));
-            }
-            catch
+        private static void SetCurrentEncryptionProvider()
+        {
+            if (_iEncryptionProviderName == null || _iEncryptionProviderProviderName == null)
             {
                 return;
             }
@@ -35,54 +28,28 @@ namespace XSerializer
             {
                 AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += AppDomainOnReflectionOnlyAssemblyResolve;
 
-                var encryptionProviderTypes = new List<string>();
+                var prioritizedGroupsOfCandidateTypes =
+                    GetAssemblyFiles()
+                        .SelectMany(GetCandidateTypes)
+                        .GroupBy(x => x.Priority, item => item.Type)
+                        .OrderByDescending(g => g.Key);
 
-                foreach (var file in files)
+                foreach (var candidateTypes in prioritizedGroupsOfCandidateTypes.Select(g => g.ToList()))
                 {
-                    try
+                    if (candidateTypes.Count != 1)
                     {
-                        var assembly = Assembly.ReflectionOnlyLoadFrom(file);
-
-                        if (assembly.FullName == typeof(ModuleInitializer).Assembly.FullName)
-                        {
-                            continue;
-                        }
-
-                        encryptionProviderTypes.AddRange(
-                            assembly.GetTypes()
-                                .Where(t =>
-                                    t.IsClass
-                                    && !t.IsAbstract
-                                    && t.IsPublic
-                                    && t.AssemblyQualifiedName != null
-                                    && t.GetInterfaces().Any(i => i.AssemblyQualifiedName == iEncryptionProviderProviderName)
-                                    && t.GetConstructor(Type.EmptyTypes) != null)
-                                .Select(t => t.AssemblyQualifiedName));
+                        WriteToEventLog(candidateTypes);
+                        continue;
                     }
-                    catch
+
+                    var encryptionProvider = GetEncryptionProvider(candidateTypes[0]);
+
+                    if (encryptionProvider != null)
                     {
+                        EncryptionProvider.Current = encryptionProvider;
+                        return;
                     }
                 }
-
-                if (encryptionProviderTypes.Count == 1)
-                {
-                    IEncryptionProviderProvider encryptionProviderProvider = null;
-
-                    var encryptionProviderType = Type.GetType(encryptionProviderTypes[0]);
-
-                    if (encryptionProviderType != null)
-                    {
-                        encryptionProviderProvider = (IEncryptionProviderProvider)Activator.CreateInstance(encryptionProviderType);
-                    }
-
-                    if (encryptionProviderProvider != null)
-                    {
-                        EncryptionProvider.Current = encryptionProviderProvider.GetEncryptionProvider();
-                    }
-                }
-            }
-            catch
-            {
             }
             finally
             {
@@ -93,6 +60,153 @@ namespace XSerializer
         private static Assembly AppDomainOnReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
         {
             return Assembly.ReflectionOnlyLoad(args.Name);
+        }
+
+        private static IEnumerable<string> GetAssemblyFiles()
+        {
+            try
+            {
+                return
+                    Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+                        .Concat(Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.exe"));
+            }
+            catch
+            {
+                return Enumerable.Empty<string>();
+            }
+        }
+
+        private static IEnumerable<PrioritizedType> GetCandidateTypes(string assemblyFile)
+        {
+            try
+            {
+                var assembly = Assembly.ReflectionOnlyLoadFrom(assemblyFile);
+
+                if (assembly.FullName == typeof(ModuleInitializer).Assembly.FullName)
+                {
+                    return Enumerable.Empty<PrioritizedType>();
+                }
+
+                return
+                    assembly.GetTypes()
+                        .Where(t =>
+                            t.IsClass
+                            && !t.IsAbstract
+                            && t.IsPublic
+                            && t.AssemblyQualifiedName != null
+                            && t.GetInterfaces().Any(i =>
+                                i.AssemblyQualifiedName == _iEncryptionProviderProviderName
+                                || i.AssemblyQualifiedName == _iEncryptionProviderName)
+                            && t.HasDefaultishConstructor())
+                        .Select(t => GetPrioritizedType(t.AssemblyQualifiedName))
+                        .Where(x => x != null);
+            }
+            catch
+            {
+                return Enumerable.Empty<PrioritizedType>();
+            }
+        }
+
+        private static bool HasDefaultishConstructor(this Type type)
+        {
+            return
+                type.GetConstructor(Type.EmptyTypes) != null
+                || type.GetConstructors().Any(ctor => ctor.GetParameters().All(HasDefaultValue));
+        }
+
+        private static bool HasDefaultValue(ParameterInfo parameter)
+        {
+            const ParameterAttributes hasDefaultValue =
+                ParameterAttributes.HasDefault | ParameterAttributes.Optional;
+
+            return (parameter.Attributes & hasDefaultValue) == hasDefaultValue;
+        }
+
+        private static PrioritizedType GetPrioritizedType(string assemblyQualifiedName)
+        {
+            try
+            {
+                var type = Type.GetType(assemblyQualifiedName);
+
+                if (type == null)
+                {
+                    return null;
+                }
+
+                var encryptionProviderAttribute =
+                    (EncryptionProviderAttribute)Attribute.GetCustomAttribute(type, typeof(EncryptionProviderAttribute));
+
+                var priority =
+                    encryptionProviderAttribute != null // If EncryptionProviderAttribute was specified...
+                        ? encryptionProviderAttribute.Priority // ...use its priority.
+                        : type.GetInterfaces().Any(i => i.AssemblyQualifiedName == _iEncryptionProviderProviderName)
+                            ? -1 // IEncryptionProviderProvider has a higher default priority...
+                            : -2; // ...than IEncryptionProvider.
+
+                return new PrioritizedType
+                {
+                    Type = type,
+                    Priority = priority
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static void WriteToEventLog(List<Type> duplicatePriorityTypes)
+        {
+            // TODO: Implement
+        }
+
+        private static IEncryptionProvider GetEncryptionProvider(Type candidateType)
+        {
+            try
+            {
+                object instance;
+
+                if (candidateType.GetConstructor(Type.EmptyTypes) != null)
+                {
+                    instance = Activator.CreateInstance(candidateType);
+                }
+                else
+                {
+                    var ctor =
+                        candidateType.GetConstructors()
+                            .OrderByDescending(c => c.GetParameters().Length)
+                            .First(c => c.GetParameters().All(HasDefaultValue));
+
+                    var args = ctor.GetParameters().Select(p => p.DefaultValue).ToArray();
+
+                    instance = Activator.CreateInstance(candidateType, args);
+                }
+
+                var encryptionProviderProvider = instance as IEncryptionProviderProvider;
+                if (encryptionProviderProvider != null)
+                {
+                    return encryptionProviderProvider.GetEncryptionProvider();
+                }
+
+                var encryptionProvider = instance as IEncryptionProvider;
+                if (encryptionProvider != null)
+                {
+                    return encryptionProvider;
+                }
+
+                // How did we even get here? Answer me that, Mr. Compiler!
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private class PrioritizedType
+        {
+            public Type Type { get; set; }
+            public int Priority { get; set; }
         }
     }
 }
