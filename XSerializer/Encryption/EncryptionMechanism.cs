73393ca52a0d3b0a4ed09d0da96e08a8d39f88af
Brian Friesen <brianfriesen@quickenloans.com>
2014-12-28 23:42:51 -0500

    Use correct casing on public static field.

--- XSerializer/Encryption/EncryptionMechanism.cs
+++ XSerializer/Encryption/EncryptionMechanism.cs
@@ -10,20 +10,20 @@
         /// <summary>
         /// The default instance of <see cref="IEncryptionMechanism"/>.
         /// </summary>
-        public static readonly IEncryptionMechanism _defaultEncryptionMechanism = new ClearTextEncryptionMechanism();
+        public static readonly IEncryptionMechanism DefaultEncryptionMechanism = new ClearTextEncryptionMechanism();
 
-        private static IEncryptionMechanism _current = _defaultEncryptionMechanism;
+        private static IEncryptionMechanism _current = DefaultEncryptionMechanism;
 
         /// <summary>
         /// Gets or sets the instance of <see cref="IEncryptionMechanism"/>
         /// to be used by XSerializer when encrypting or decrypting data.
         /// When setting this property, if <paramref name="value"/> is null,
-        /// then <see cref="_defaultEncryptionMechanism"/> will be used instead.
+        /// then <see cref="DefaultEncryptionMechanism"/> will be used instead.
         /// </summary>
         public static IEncryptionMechanism Current
         {
             internal get { return _current; }
-            set { _current = value ?? _defaultEncryptionMechanism; }
+            set { _current = value ?? DefaultEncryptionMechanism; }
         }
     }
 }
