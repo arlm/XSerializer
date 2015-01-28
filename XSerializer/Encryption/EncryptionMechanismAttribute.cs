af056f4d1b09a8e6854e7b34bbba03ad37eddb6f
Brian Friesen <brianfriesen@quickenloans.com>
2015-01-28 11:54:24 -0500

    Update Rock.StaticDependencyInjection nuget package.

--- XSerializer/Encryption/EncryptionMechanismAttribute.cs
+++ XSerializer/Encryption/EncryptionMechanismAttribute.cs
@@ -5,90 +5,80 @@ namespace XSerializer.Encryption
     /// <summary>
     /// Indicates that a class should be used to set the value of
     /// <see cref="EncryptionMechanism.Current"/>. In order to be used,
-    /// a non-abstract class decorated by this attribute (or, if this attribute 
-    /// decorates an assembly, the type indicated by the <see cref="ClassType"/>
-    /// property) must implement either <see cref="IEncryptionMechanism"/> or 
-    /// <see cref="IEncryptionMechanismFactory"/> and provide either a public 
-    /// parameterless constructor or a constructor whose parameters are all
-    /// optional (have a default value).
+    /// a non-abstract class decorated by this attribute must implement either
+    /// <see cref="IEncryptionMechanism"/> or <see cref="IEncryptionMechanismFactory"/>
+    /// and provide either a public parameterless constructor or a constructor
+    /// whose parameters are all optional (have a default value).
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class)]
     public sealed class EncryptionMechanismAttribute : Attribute
     {
-        private readonly Type _classType;
         private readonly int _priority;
+        private readonly bool _disabled;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class.
-        /// This constructor should only be used when this attribute decorates a class.
+        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class
+        /// with its <see cref="Priority"/> property set to zero and its <see cref="Disabled"/>
+        /// property set to false.
         /// </summary>
         public EncryptionMechanismAttribute()
-            : this(0)
+            : this(0, false)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class.
-        /// This constructor should only be used when this attribute decorates a class.
+        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class
+        /// with its <see cref="Priority"/> property set to the value of the 
+        /// <paramref name="priority"/> parameter and its <see cref="Disabled"/> property set
+        /// to false.
         /// </summary>
-        /// <param name="priority">The priority of the class.</param>
+        /// <param name="priority">A value that indicates the priority of a class. This value is used
+        /// when multiple implementations of <see cref="IEncryptionMechanism"/> or
+        /// <see cref="IEncryptionMechanismFactory"/> are discovered. The one with
+        /// the highest priority will be used.</param>
         public EncryptionMechanismAttribute(int priority)
-            : this(null, priority)
+            : this(priority, false)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class.
-        /// This constructor should only be used when this attribute decorates an assembly.
+        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class
+        /// with its <see cref="Priority"/> property set to the value of the 
+        /// <paramref name="priority"/> parameter and its <see cref="Disabled"/> property set
+        /// to the value of the <paramref name="disabled"/> parameter.
         /// </summary>
-        /// <param name="classType">
-        /// The type of the class to export. If this attribute decorates a class, then
-        /// this value will be ignored.
+        /// <param name="priority">A value that indicates the priority of a class. This value is used
+        /// when multiple implementations of <see cref="IEncryptionMechanism"/> or
+        /// <see cref="IEncryptionMechanismFactory"/> are discovered. The one with
+        /// the highest priority will be used.</param>
+        /// <param name="disabled">
+        /// Whether the class that this attribute decorates should excluded from consideration for
+        /// an import operation.
         /// </param>
-        public EncryptionMechanismAttribute(Type classType)
-            : this(classType, 0)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EncryptionMechanismAttribute"/> class.
-        /// This constructor should only be used when this attribute decorates an assembly.
-        /// </summary>
-        /// <param name="classType">
-        /// The type of the class to export. If this attribute decorates a class, then
-        /// this value will be ignored.
-        /// </param>
-        /// <param name="priority">The priority of the class.</param>
-        public EncryptionMechanismAttribute(Type classType, int priority)
+        public EncryptionMechanismAttribute(int priority, bool disabled)
         {
             _priority = priority;
-            _classType = classType;
+            _disabled = disabled;
         }
 
         /// <summary>
-        /// Gets the type of class to export. Only used when this attribute decorates an assembly.
+        /// Gets a value that indicates the priority of a class. This value is used
+        /// when multiple implementations of <see cref="IEncryptionMechanism"/> or
+        /// <see cref="IEncryptionMechanismFactory"/> are discovered. The one with
+        /// the highest priority will be used.
         /// </summary>
-        public Type ClassType { get { return _classType; } }
-
-        /// <summary>
-        /// Gets a value that indicates this class's relative priority.
-        /// </summary>
-        public int Priority { get { return _priority; } }
-
-        /// <summary>
-        /// Gets a value indicating whether this class is explicitly ineligible 
-        /// for to be used as the encryption mechanism for the current app domain.
-        /// </summary>
-        public bool Disabled { get; set; }
-
-        /// <summary>
-        /// Gets a value indicating whether this instance is valid as an assembly attribute.
-        /// Returns true if <see cref="ClassType"/> is not null and describes a class (e.g.
-        /// not an interface or enum). Else, false.
-        /// </summary>
-        internal bool IsValidForAssemblyAttribute
+        public int Priority
         {
-            get { return ClassType != null && ClassType.IsClass; }
+            get { return _priority; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the class that this attribute decorates
+        /// should excluded from consideration for an import operation.
+        /// </summary>
+        public bool Disabled
+        {
+            get { return _disabled; }
         }
     }
 }
