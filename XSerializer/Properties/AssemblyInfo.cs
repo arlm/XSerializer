11bbd42d7dc1064bd0feab948567cc3bbddea0c6
Brian Friesen <brianfriesen@quickenloans.com>
2015-05-08 21:13:58 -0400

    Add strong naming back, mark assembly as cls compliant.

--- XSerializer/Properties/AssemblyInfo.cs
+++ XSerializer/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,6 +23,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5aeaebd0-35de-424c-baa9-ba6d65fa3409")]
 
+[assembly: CLSCompliant(true)]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -36,5 +39,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("0.2.0")]
 [assembly: AssemblyInformationalVersion("0.2.0-beta01")]
 
-[assembly: InternalsVisibleTo("XSerializer.Tests")]
-[assembly: InternalsVisibleTo("XSerializer.PerformanceTests")]
+[assembly: InternalsVisibleTo("XSerializer.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006560e0525bf37e236bd894c2df8b1c6b4157cd615f3bb8a944994ed4cf969fba107ed2f755ca796a28e7f94bb85974ca95fc93e87eb15583badda9199fa9dd19d2a8c0c16b2ecbc9587463cbc8a1672c30c145edec9950a350738673f5010780a261570a5a0861d247560dd224bab51504dff66c31dc003a5bc61240bc513889")]
+[assembly: InternalsVisibleTo("XSerializer.PerformanceTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002b3edca890a00e709a42ff8643cf9c4e7d0060a3a858c02a776036d86cb5cae0ed91684c0df8b34f6da547c8a9617150afc13822d5a5b0ab94105ba0576a55e4e8230d01475d889ed9eb138be76dec6894d1fe4c4890aa3e12430698078ae1d8b27aa24675576792032de51c83901f90b78d732e54b2931c26e868f00295c6a0")]
