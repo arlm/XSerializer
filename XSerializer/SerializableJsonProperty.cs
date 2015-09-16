25e063359d3902ddc0efe29f7fb560375b11daf6
Brian Friesen <brianfriesen@quickenloans.com>
2015-09-16 11:12:21 -0400

    Optimize SerializableJsonProperty delegates

--- XSerializer/SerializableJsonProperty.cs
+++ XSerializer/SerializableJsonProperty.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace XSerializer
@@ -12,12 +13,20 @@ namespace XSerializer
 
         public SerializableJsonProperty(PropertyInfo propertyInfo, bool encrypt)
         {
+            if (propertyInfo.DeclaringType == null)
+            {
+                throw new ArgumentException("The DeclaringType of the PropertyInfo must not be null.", "propertyInfo");
+            }
+
             _name = propertyInfo.Name;
             _serializer = new Lazy<IJsonSerializerInternal>(() => JsonSerializerFactory.GetSerializer(propertyInfo.PropertyType, encrypt));
 
-            // TODO: Optimize these delegates.
-            _getValue = instance => propertyInfo.GetValue(instance, null);
-            _setValue = (instance, value) => propertyInfo.SetValue(instance, value, null);
+            _getValue = GetGetValueFunc(propertyInfo, propertyInfo.DeclaringType);
+
+            if (propertyInfo.CanWrite)
+            {
+                _setValue = GetSetValueAction(propertyInfo, propertyInfo.DeclaringType);
+            }
         }
 
         public string Name
@@ -42,6 +51,33 @@ namespace XSerializer
         {
             var value = ReadValue(reader, info);
             _setValue(instance, value);
+        }
+
+        private static Func<object, object> GetGetValueFunc(PropertyInfo propertyInfo, Type declaringType)
+        {
+            var instanceParameter = Expression.Parameter(typeof(object), "instance");
+
+            Expression property = Expression.Property(Expression.Convert(instanceParameter, declaringType), propertyInfo);
+
+            if (propertyInfo.PropertyType.IsValueType) // Needs boxing.
+            {
+                property = Expression.Convert(property, typeof(object));
+            }
+
+            var lambda = Expression.Lambda<Func<object, object>>(property, new[] { instanceParameter });
+            return lambda.Compile();
+        }
+
+        private static Action<object, object> GetSetValueAction(PropertyInfo propertyInfo, Type declaringType)
+        {
+            var instanceParameter = Expression.Parameter(typeof(object), "instance");
+            var valueParameter = Expression.Parameter(typeof(object), "value");
+
+            Expression property = Expression.Property(Expression.Convert(instanceParameter, declaringType), propertyInfo);
+            var assign = Expression.Assign(property, Expression.Convert(valueParameter, propertyInfo.PropertyType));
+
+            var lambda = Expression.Lambda<Action<object, object>>(assign, new[] { instanceParameter, valueParameter });
+            return lambda.Compile();
         }
     }
 }
