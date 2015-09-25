f1f09bb05ea76ae6eb6674d430b44ab26ecc349d
Brian Friesen <brianfriesen@quickenloans.com>
2015-09-25 12:43:20 -0400

    Build prerelease package
    Published internally.

--- XSerializer/Properties/AssemblyInfo.cs
+++ XSerializer/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
-[assembly: AssemblyInformationalVersion("0.3.0-alpha07")]
+[assembly: AssemblyInformationalVersion("0.3.0-alpha08")]
 
 [assembly: InternalsVisibleTo("XSerializer.Tests")]
 [assembly: InternalsVisibleTo("XSerializer.PerformanceTests")]
