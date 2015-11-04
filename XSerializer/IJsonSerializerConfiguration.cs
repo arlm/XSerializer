7244dc4fc4f6d2962bbe263dbca5b0388653f491
Brian Friesen <brianfriesen@quickenloans.com>
2015-11-03 17:55:14 -0800

    Add xml documentation

--- XSerializer/IJsonSerializerConfiguration.cs
+++ XSerializer/IJsonSerializerConfiguration.cs
@@ -7,16 +7,50 @@ using XSerializer.Encryption;
 namespace XSerializer
 {
     /// <summary>
-    /// 
+    /// Contains various configuration settings required by instances of <see cref="JsonSerializer{T}"/>.
     /// </summary>
     public interface IJsonSerializerConfiguration
     {
+        /// <summary>
+        /// Gets the <see cref="Encoding"/> to be used for serialization operations.
+        /// </summary>
         Encoding Encoding { get; }
+
+        /// <summary>
+        /// Gets an instance of <see cref="IEncryptionMechanism"/> to be used to encrypted properties
+        /// decorated with the <see cref="EncryptAttribute"/> attribute. If null, those properties
+        /// will not be encryped.d
+        /// </summary>
         IEncryptionMechanism EncryptionMechanism { get; }
+
+        /// <summary>
+        /// Gets an object to be used to retrieve encryption settings for an encryption operation.
+        /// </summary>
         object EncryptKey { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the root object should be encrypted in its entirety.
+        /// </summary>
         bool EncryptRootObject { get; }
+
+        /// <summary>
+        /// Gets an object used to create <see cref="DateTime"/> values from a string representation.
+        /// </summary>
         IDateTimeHandler DateTimeHandler { get; }
+
+        /// <summary>
+        /// Gets a dictionary for type-to-type mappings, where the value type is assignable to the key
+        /// type. This is typically a interface-to-implementation or astract-class-to-inheritor
+        /// relationship, although a concrete-class-to-inheritor relationship is also possible.
+        /// </summary>
         IDictionary<Type, Type> ConcreteImplementationsByType { get; }
+
+        /// <summary>
+        /// Gets a dictionary for property-to-type mappings, where the value type is assignable to
+        /// the key property's type. This is typically a interface-to-implementation or
+        /// astract-class-to-inheritor relationship, although a concrete-class-to-inheritor relationship
+        /// is also possible.
+        /// </summary>
         IDictionary<PropertyInfo, Type> ConcreteImplementationsByProperty { get; }
     }
 }
--- XSerializer/JsonSerializer.cs
+++ XSerializer/JsonSerializer.cs
@@ -7,15 +7,41 @@ using XSerializer.Encryption;
 
 namespace XSerializer
 {
+    /// <summary>
+    /// A factory class that creates instances of the <see cref="IXSerializer"/> interface.
+    /// </summary>
+    /// <remarks>
+    /// An instance of the generic <see cref="JsonSerializer{T}"/> class is returned from
+    /// each method in this class.
+    /// </remarks>
     public static class JsonSerializer
     {
         private static readonly ConcurrentDictionary<Type, Func<IJsonSerializerConfiguration, IXSerializer>> _createXmlSerializerFuncs = new ConcurrentDictionary<Type, Func<IJsonSerializerConfiguration, IXSerializer>>();
 
+        /// <summary>
+        /// Create an instance of IXmlSerializer for the given type using a default configuration.
+        /// </summary>
+        /// <param name="type">The type of the object that the serializer will operate on.</param>
+        /// <returns>An instance of IXmlSerializer.</returns>
+        /// <remarks>
+        /// An instance of the generic <see cref="JsonSerializer{T}"/> class of type <paramref name="type"/>
+        /// is returned from this method.
+        /// </remarks>
         public static IXSerializer Create(Type type)
         {
             return Create(type, null);
         }
 
+        /// <summary>
+        /// Create an instance of IXmlSerializer for the given type using the specified configuration.
+        /// </summary>
+        /// <param name="type">The type of the object that the serializer will operate on.</param>
+        /// <param name="configuration">The configuration for the serializer.</param>
+        /// <returns>An instance of IXmlSerializer.</returns>
+        /// <remarks>
+        /// An instance of the generic <see cref="JsonSerializer{T}"/> class of type <paramref name="type"/>
+        /// is returned from this method.
+        /// </remarks>
         public static IXSerializer Create(Type type, IJsonSerializerConfiguration configuration)
         {
             var createJsonSerializer = _createXmlSerializerFuncs.GetOrAdd(
@@ -39,16 +65,29 @@ namespace XSerializer
         }
     }
 
+    /// <summary>
+    /// An object used for serializing and deserializing objects of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of object to serialize and deserialize.</typeparam>
     public class JsonSerializer<T> : IXSerializer
     {
         private readonly IJsonSerializerConfiguration _configuration;
         private readonly IJsonSerializerInternal _serializer;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonSerializer{T}"/> class using a
+        /// default configuration.
+        /// </summary>
         public JsonSerializer()
             : this(new JsonSerializerConfiguration())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonSerializer{T}"/> class using the 
+        /// specified configuration.
+        /// </summary>
+        /// <param name="configuration"></param>
         public JsonSerializer(IJsonSerializerConfiguration configuration)
         {
             _configuration = configuration;
@@ -60,6 +99,11 @@ namespace XSerializer
             _serializer = JsonSerializerFactory.GetSerializer(typeof(T), encrypt, _configuration.ConcreteImplementationsByType, _configuration.ConcreteImplementationsByProperty);
         }
 
+        /// <summary>
+        /// Serialize the given object to a string.
+        /// </summary>
+        /// <param name="instance">The object to serialize.</param>
+        /// <returns>A string representation of the object.</returns>
         string IXSerializer.Serialize(object instance)
         {
             var sb = new StringBuilder();
@@ -72,11 +116,21 @@ namespace XSerializer
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Serialize the given object to a JSON string.
+        /// </summary>
+        /// <param name="instance">The object to serialize.</param>
+        /// <returns>A JSON string representation of the object.</returns>
         public string Serialize(T instance)
         {
             return ((IXSerializer)this).Serialize(instance);
         }
 
+        /// <summary>
+        /// Serialize the given object to the given <see cref="Stream"/> as JSON.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to serialize the object to.</param>
+        /// <param name="instance">The object to serialize.</param>
         void IXSerializer.Serialize(Stream stream, object instance)
         {
             using (var writer = new StreamWriter(stream, _configuration.Encoding))
@@ -85,11 +139,21 @@ namespace XSerializer
             }
         }
 
+        /// <summary>
+        /// Serialize the given object to the given <see cref="Stream"/> as JSON.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to serialize the object to.</param>
+        /// <param name="instance">The object to serialize.</param>
         public void Serialize(Stream stream, T instance)
         {
             ((IXSerializer)this).Serialize(stream, instance);
         }
 
+        /// <summary>
+        /// Serialize the given object to the given <see cref="Stream"/> as JSON.
+        /// </summary>
+        /// <param name="textWriter">The <see cref="TextWriter"/> to serialize the object to.</param>
+        /// <param name="instance">The object to serialize.</param>
         void IXSerializer.Serialize(TextWriter textWriter, object instance)
         {
             var info = GetJsonSerializeOperationInfo();
@@ -100,11 +164,21 @@ namespace XSerializer
             }
         }
 
+        /// <summary>
+        /// Serialize the given object to the given <see cref="Stream"/> as JSON.
+        /// </summary>
+        /// <param name="textWriter">The <see cref="TextWriter"/> to serialize the object to.</param>
+        /// <param name="instance">The object to serialize.</param>
         public void Serialize(TextWriter textWriter, T instance)
         {
             ((IXSerializer)this).Serialize(textWriter, instance);
         }
 
+        /// <summary>
+        /// Deserialize an object from a JSON string.
+        /// </summary>
+        /// <param name="json">A JSON representation of an object.</param>
+        /// <returns>An object created from the JSON string.</returns>
         object IXSerializer.Deserialize(string json)
         {
             using (var reader = new StringReader(json))
@@ -113,11 +187,24 @@ namespace XSerializer
             }
         }
 
+        /// <summary>
+        /// Deserialize an object from a JSON representation of that object.
+        /// </summary>
+        /// <param name="json">A JSON representation of an object.</param>
+        /// <returns>An object created from the JSON string.</returns>
         public T Deserialize(string json)
         {
             return (T)((IXSerializer)this).Deserialize(json);
         }
 
+        /// <summary>
+        /// Deserialize an object from a <see cref="Stream"/> containing a JSON representation
+        /// of that object.
+        /// </summary>
+        /// <param name="stream">
+        /// A <see cref="Stream"/> that when read, contains a representation of an object.
+        /// </param>
+        /// <returns>An object created from the <see cref="Stream"/>.</returns>
         object IXSerializer.Deserialize(Stream stream)
         {
             using (var reader = new StreamReader(stream, _configuration.Encoding))
@@ -126,11 +213,27 @@ namespace XSerializer
             }
         }
 
+        /// <summary>
+        /// Deserialize an object from a <see cref="Stream"/> containing a JSON representation
+        /// of that object.
+        /// </summary>
+        /// <param name="stream">
+        /// A <see cref="Stream"/> that when read, contains a representation of an object.
+        /// </param>
+        /// <returns>An object created from the <see cref="Stream"/>.</returns>
         public T Deserialize(Stream stream)
         {
             return (T)((IXSerializer)this).Deserialize(stream);
         }
 
+        /// <summary>
+        /// Deserialize an object from a <see cref="Stream"/> containing a JSON representation
+        /// of that object.
+        /// </summary>
+        /// <param name="textReader">
+        /// A <see cref="TextReader"/> that when read, contains a representation of an object.
+        /// </param>
+        /// <returns>An object created from the <see cref="TextReader"/>.</returns>
         object IXSerializer.Deserialize(TextReader textReader)
         {
             var info = GetJsonSerializeOperationInfo();
@@ -141,6 +244,14 @@ namespace XSerializer
             }
         }
 
+        /// <summary>
+        /// Deserialize an object from a <see cref="Stream"/> containing a JSON representation
+        /// of that object.
+        /// </summary>
+        /// <param name="textReader">
+        /// A <see cref="TextReader"/> that when read, contains a representation of an object.
+        /// </param>
+        /// <returns>An object created from the <see cref="TextReader"/>.</returns>
         public T Deserialize(TextReader textReader)
         {
             return (T)((IXSerializer)this).Deserialize(textReader);
