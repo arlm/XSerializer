45e515261261dd4bb71ed3259b3b013cb10d471d
Brian Friesen <brianfriesen@quickenloans.com>
2016-02-24 16:13:34 -0500

    Add tests (and fix found bugs) for DefaultDateTimeHandler

--- XSerializer/DefaultDateTimeHandler.cs
+++ XSerializer/DefaultDateTimeHandler.cs
@@ -90,8 +90,23 @@ namespace XSerializer
 
             GetDateTimeComponents(value, out year, out month, out day, out hour, out minute, out second, out ticks, out offsetHours, out offsetMinutes, out dateTimeKind);
 
-            var offset = new TimeSpan(offsetHours, offsetMinutes, 0);
-            var dateTimeOffset = new DateTimeOffset(year, month, day, hour, minute, second, offset);
+            TimeSpan utcOffset;
+
+            switch (dateTimeKind)
+            {
+                case DateTimeKind.Unspecified:
+                    utcOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
+                    break;
+                case DateTimeKind.Utc:
+                case DateTimeKind.Local:
+                    utcOffset = new TimeSpan(offsetHours, offsetMinutes, 0);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            
+            var dateTimeOffset = new DateTimeOffset(year, month, day, hour, minute, second, utcOffset);
             dateTimeOffset = dateTimeOffset.AddTicks(ticks);
 
             return dateTimeOffset;
@@ -110,92 +125,156 @@ namespace XSerializer
             out int offsetMinutes,
             out DateTimeKind dateTimeKind)
         {
-            year = 0;
-            month = 0;
-            day = 0;
-            hour = 0;
-            minute = 0;
-            second = 0;
+            year = int.MinValue;
+            month = int.MinValue;
+            day = int.MinValue;
+            hour = int.MinValue;
+            minute = int.MinValue;
+            second = int.MinValue;
             offsetHours = 0;
             offsetMinutes = 0;
             dateTimeKind = DateTimeKind.Unspecified;
 
-            double fraction = 0;
+            double? fraction = null;
 
-            for (int i = 0; i < _dateFormat.Length && i < value.Length; i++)
+            for (int valueIndex = 0, formatIndex = 0; formatIndex < _dateFormat.Length && valueIndex < value.Length; valueIndex++, formatIndex++)
             {
-                char c = value[i];
+                char c = value[valueIndex];
 
-                switch (_dateFormat[i])
+                switch (_dateFormat[formatIndex])
                 {
                     case 'y':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "year");
                         year = year * 10 + (c - '0');
                         break;
                     case 'M':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "month");
                         month = month * 10 + (c - '0');
                         break;
                     case 'd':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "day");
                         day = day * 10 + (c - '0');
                         break;
                     case 'H':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "hours");
                         hour = hour * 10 + (c - '0');
                         break;
                     case 'm':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "minutes");
                         minute = minute * 10 + (c - '0');
                         break;
                     case 's':
-                        ThrowIfNotDigit(c);
+                        ThrowIfNotDigit(c, valueIndex, value, "seconds");
                         second = second * 10 + (c - '0');
                         break;
                     case '.':
-                        fraction = ParseFraction(value, ref i);
+                        if (value[valueIndex] == '.')
+                        {
+                            fraction = ParseFraction(value, ref valueIndex, ref formatIndex);
+                        }
+                        else
+                        {
+                            valueIndex--;
+                            formatIndex++;
+                            AdvancePastFractionalPart(ref formatIndex);
+                        }
                         break;
                     case 'K':
-                        ParseOffset(value, i, out offsetHours, out offsetMinutes, out dateTimeKind);
+                        ParseOffset(value, valueIndex, out offsetHours, out offsetMinutes, out dateTimeKind);
                         break;
                     case '-':
                     case 'T':
                     case ':':
-                        if (value[i] != c)
+                        if (_dateFormat[valueIndex] != c)
                         {
-                            throw new Exception();
+                            throw new FormatException(
+                                string.Format("Expected '{0}' but was '{1}' at index {2} in '{3}'.",
+                                _dateFormat[valueIndex], c, valueIndex, value));
                         }
                         break;
                 }
             }
 
-            ticks = (long)(fraction * TimeSpan.TicksPerSecond);
+            if (year == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing year in date time '{0}'.", value));
+            }
+
+            if (month == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing month in date time '{0}'.", value));
+            }
+
+            if (day == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing day in date time '{0}'.", value));
+            }
+
+            if (hour == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing hour in date time '{0}'.", value));
+            }
+
+            if (minute == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing minute in date time '{0}'.", value));
+            }
+
+            if (second == int.MinValue)
+            {
+                throw new FormatException(string.Format("Missing second in date time '{0}'.", value));
+            }
+
+            ticks = fraction == null ? 0 : (long)(fraction * TimeSpan.TicksPerSecond);
         }
 
-        private static double ParseFraction(string sourceString, ref int i)
+        private static double ParseFraction(string sourceString, ref int valueIndex, ref int formatIndex)
         {
             int numerator = 0;
             int denominator = 1;
 
-            for (i = i + 1; i < _dateFormat.Length && i < sourceString.Length; i++)
+            for (valueIndex = valueIndex + 1, formatIndex = formatIndex + 1; formatIndex < _dateFormat.Length && valueIndex < sourceString.Length; valueIndex++, formatIndex++)
             {
-                char c = sourceString[i];
+                char c = sourceString[valueIndex];
 
-                if (_dateFormat[i] == 'f')
+                if (_dateFormat[formatIndex] == 'f')
                 {
-                    ThrowIfNotDigit(c);
+                    if (!char.IsDigit(c))
+                    {
+                        valueIndex--;
+                        break;
+                    }
+
                     numerator = numerator * 10 + (c - '0');
                     denominator *= 10;
                 }
                 else
                 {
-                    i--;
+                    valueIndex--;
                     break;
                 }
             }
 
+            if (denominator == 1)
+            {
+                throw new FormatException(string.Format(
+                    "Missing decimal digits after '.' in {0}.", sourceString));
+            }
+
+            AdvancePastFractionalPart(ref formatIndex);
+
             var fraction = numerator / (double)denominator;
             return fraction;
+        }
+
+        private static void AdvancePastFractionalPart(ref int formatIndex)
+        {
+            while (_dateFormat[formatIndex] == 'f')
+            {
+                formatIndex++;
+            }
+
+            formatIndex--;
         }
 
         private static void ParseOffset(
@@ -234,23 +313,27 @@ namespace XSerializer
             }
             else
             {
-                throw new Exception();
+                throw new FormatException(
+                    string.Format("Expected start of timezone but was '{0}' at index {1} in '{2}'.",
+                    sourceString[i], i, sourceString));
             }
 
             dateTimeKind = DateTimeKind.Local;
 
-            ThrowIfNotDigit(sourceString[i + 1]);
-            ThrowIfNotDigit(sourceString[i + 2]);
-            hours = hours * 10 + (sourceString[i + 1] - '0');
-            hours = hours * 10 + (sourceString[i + 2] - '0');
-
-            if (sourceString[i + 3] != ':')
+            if (sourceString.Length < i + 5
+                || !char.IsDigit(sourceString[i + 1])
+                || !char.IsDigit(sourceString[i + 2])
+                || sourceString[i + 3] != ':'
+                || !char.IsDigit(sourceString[i + 4])
+                || !char.IsDigit(sourceString[i + 5]))
             {
-                throw new Exception();
+                throw new FormatException(
+                    string.Format("Expected timezone offset but was '{0}' at index {1} in '{2}'.",
+                    sourceString.Substring(i), i, sourceString));
             }
 
-            ThrowIfNotDigit(sourceString[i + 4]);
-            ThrowIfNotDigit(sourceString[i + 5]);
+            hours = hours * 10 + (sourceString[i + 1] - '0');
+            hours = hours * 10 + (sourceString[i + 2] - '0');
             minutes = minutes * 10 + (sourceString[i + 4] - '0');
             minutes = minutes * 10 + (sourceString[i + 5] - '0');
 
@@ -348,11 +431,13 @@ namespace XSerializer
         }
 
         // ReSharper disable once UnusedParameter.Local
-        private static void ThrowIfNotDigit(char c)
+        private static void ThrowIfNotDigit(char c, int i, string sourceString, string dateTimePart)
         {
             if (!char.IsDigit(c))
             {
-                throw new Exception();
+                throw new FormatException(
+                    string.Format("Expected numeric {0} but was '{1}' at index {2} in '{3}'.",
+                    dateTimePart, c, i, sourceString));
             }
         }
     }
