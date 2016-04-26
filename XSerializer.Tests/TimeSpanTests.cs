24bf30dd720a994874b372bca9f9bd720bf375c0
Brian Friesen <brianfriesen@quickenloans.com>
2016-04-25 23:47:26 -0400

    Add json support for TimeSpan

--- XSerializer.Tests/TimeSpanTests.cs
+++ XSerializer.Tests/TimeSpanTests.cs
@@ -26,6 +26,27 @@ namespace XSerializer.Tests
             Assert.That(roundTripFoo.Qux, Is.EqualTo(foo.Qux));
         }
 
+        [Test]
+        public void TimeSpanValuesRoundTripCorrectlyForJson()
+        {
+            var foo = new Foo
+            {
+                Bar = TimeSpan.FromSeconds(1),
+                Baz = TimeSpan.FromSeconds(2)
+            };
+
+            var serializer = new JsonSerializer<Foo>();
+
+            var json = serializer.Serialize(foo);
+            Console.WriteLine(json);
+
+            var roundTripFoo = serializer.Deserialize(json);
+
+            Assert.That(roundTripFoo.Bar, Is.EqualTo(foo.Bar));
+            Assert.That(roundTripFoo.Baz, Is.EqualTo(foo.Baz));
+            Assert.That(roundTripFoo.Qux, Is.EqualTo(foo.Qux));
+        }
+
         public class Foo
         {
             public TimeSpan Bar { get; set; }
