726f3d5d5ad52585cbb83dff625e566a60eaff62
Brian Friesen <brianfriesen@quickenloans.com>
2017-01-05 22:25:09 -0500

    Fix ren command in build script
    Second parameter is the file name - the path can't be changed.

--- build/buildNuget.XSerializer.bat
+++ build/buildNuget.XSerializer.bat
@@ -1,7 +1,7 @@
 msbuild /p:Configuration=Build ..\XSerializer\XSerializer.csproj
 
 ildasm ..\XSerializer\bin\Build\XSerializer.dll /out:..\XSerializer\bin\Build\XSerializer.il
-ren ..\XSerializer\bin\Build\XSerializer.dll ..\XSerializer\bin\Build\XSerializer.dll.orig
+ren ..\XSerializer\bin\Build\XSerializer.dll XSerializer.dll.orig
 ilasm ..\XSerializer\bin\Build\XSerializer.il /res:..\XSerializer\bin\Build\XSerializer.res /dll /key=..\XSerializer\XSerializer.snk
 
 nuget pack ..\XSerializer\XSerializer.csproj -Properties Configuration=Build
