860e5a3f398f1579fb3d3703c0eb017f717dee9a
Brian Friesen <brianfriesen@quickenloans.com>
2021-04-30 14:19:47 -0400

    Demonstrate CDATA bug
    XmlText elements with CDATA values are not being picked up.

--- XSerializer.Tests/XmlBugTests.cs
+++ XSerializer.Tests/XmlBugTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Xml.Serialization;
 
 namespace XSerializer.Tests
 {
@@ -28,6 +29,21 @@ namespace XSerializer.Tests
             Assert.That(data.Person.Name.First, Is.EqualTo("Bilbo"));
             Assert.That(data.Person.Name.Last, Is.EqualTo("Baggins"));
         }
+
+        [Test]
+        public void CDATAIsSupportedForXmlText()
+        {
+            var xml = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<ErrorEvent>
+    <Message><![CDATA[<Oh, no!>]]></Message>
+</ErrorEvent>";
+
+            var serializer = new XmlSerializer<ErrorEvent>();
+
+            var errorEvent = serializer.Deserialize(xml);
+
+            Assert.That(errorEvent.Message.Value, Is.EqualTo("<Oh, no!>"));
+        }
     }
 
     public class Data
@@ -44,5 +60,16 @@ namespace XSerializer.Tests
     {
         public string First { get; set; }
         public string Last { get; set; }
+    }
+
+    public class ErrorEvent
+    {
+        public Message Message { get; set; }
+    }
+
+    public class Message
+    {
+        [XmlText]
+        public string Value { get; set; }
     }
 }
